bcf7697a809b5f982fd7a471121dd61e1985fcc4
mjbean <32068678+mjbean@users.noreply.github.com>
2017-09-18 15:17:51 -0400

    Update SFLogins.php

--- workbench/SFLogins.php
+++ workbench/SFLogins.php
@@ -1,28 +1,3 @@
 <php
-     $queryString = "SELECT Name, Account.Name FROM Contact";
-
-$queryString .= " LIMIT 1";
-
-$queryResult = $sfdc->query($queryString);
-
-  
-
- if ($queryResult->size > 0) {
-
-  $records = $queryResult->records;
-
-  foreach ($records as $record) {
-
-   $sObject = new SObject($record);
-
-   echo '<pre>';
-
-   print_r($sObject);
-
-   echo '</pre>';
-
-  }
-
- }
-
+     print($queryString = "SELECT Name, Account.Name FROM Contact");
 ?>
