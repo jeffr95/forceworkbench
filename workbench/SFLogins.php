6e83e0400042bc0dc50ee1613dfe3dafd623518d
mjbean <32068678+mjbean@users.noreply.github.com>
2017-09-18 15:36:02 -0400

    Update SFLogins.php

--- workbench/SFLogins.php
+++ workbench/SFLogins.php
@@ -4,6 +4,21 @@
      require_once 'shared.php';
      require_once 'async/QueryFutureTask.php';
      
-     $query = "SELECT Id, FirstName, LastName, Phone from Contact";
-     $response = $mySforceConnection->query($query);
+ 
+//Obtaining object containing record data with Lead ID
+$queryLead = "SELECT Id, LastName, FirstName, Salutation, Title, Company, Street, City, State, PostalCode, Country, Phone, Email, Website FROM Lead WHERE Id='00A5150409abcyQAHB'";
+$resultLead = $connection->query($queryLead);
+ 
+//Obtaining object containing record data with Email address
+$queryEmail = "SELECT Id, LastName, FirstName, Salutation, Title, Company, Street, City, State, PostalCode, Country, Phone, Email, Website FROM Lead WHERE Email='somebody@email.com'";
+$resultEmail = $connection->query($queryEmail);
+ 
+//Checking if the query for a Lead with ID returns an object
+if ($resultLead->size) {
+ //The lead exists
+ //Display or parse values from the object
+}
+else {
+ echo "This lead does not exist in Salesforce.";
+}
 ?>
