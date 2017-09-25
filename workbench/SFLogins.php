f74ce0c65f95c3c92fbe6894e45926a579e6fb30
mjbean <32068678+mjbean@users.noreply.github.com>
2017-09-25 17:04:55 -0400

    Update SFLogins.php

--- workbench/SFLogins.php
+++ workbench/SFLogins.php
@@ -1,7 +1,3 @@
-<?php
-require_once 'header.php';
-?>
-
 <php>
 <form>
   <p><input type="button" value="Login" onclick="window.open('https://www.salesforce.com/login.jsp?un=relationedgeatlanta@gmail.com&pw=Atlanta12345')"/>     RelationEdge Prod</p>
