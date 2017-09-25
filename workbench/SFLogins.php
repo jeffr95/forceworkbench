4d81842e5f7e7c477c0394384bbbf0b513eee9df
mjbean <32068678+mjbean@users.noreply.github.com>
2017-09-25 13:36:26 -0400

    Update SFLogins.php

--- workbench/SFLogins.php
+++ workbench/SFLogins.php
@@ -1,6 +1,6 @@
 <php>
 <form>
-  <input type="button" value="Login" onclick="window.location.href='https://www.salesforce.com/login.jsp?un=relationedgeatlanta@gmail.com&pw=Atlanta12345'" /><p>RelationEdge</p>
-  <input type="button" value="Login" onclick="window.location.href='https://www.salesforce.com/login.jsp?un=jrichards@relationedge.dev1.com&pw=REPassword1'" /><p>RelationEdge Dev</p>
+  <input type="button" value="Login" onclick="window.location.href='https://www.salesforce.com/login.jsp?un=relationedgeatlanta@gmail.com.gg&pw=REAtlanta2'" />GGSC Prod
+  <input type="button" value="Login" onclick="window.location.href='https://www.salesforce.com/login.jsp?un=relationedgeatlanta@gmail.com.gg.dev1&pw=REAtlanta1'" />GGSC Sandbox
 </form>
 </php>
