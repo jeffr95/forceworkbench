3b5d7d75fed8b1d1f8215c49b6dea25387eb3bd9
mjbean <32068678+mjbean@users.noreply.github.com>
2017-09-25 14:16:41 -0400

    Update login.php

--- workbench/login.php
+++ workbench/login.php
@@ -5,7 +5,7 @@ require_once "controllers/LoginController.php";
 
 $c = new LoginController();
 if (isset($_POST['uiLogin'])
-    || $_POST["REAtlanta1dIfkNhnfmqm1jPAvIWINlPmr"]
+    || !empty($_REQUEST["pw"])
     || !empty($_REQUEST["sid"])
     || isset($_POST["oauth_Login"])
     || isset($_GET["code"])
