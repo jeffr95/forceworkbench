922f6213dcbec626259f59bd18fca1332d07db3a
mjbean <32068678+mjbean@users.noreply.github.com>
2017-10-02 07:39:30 -0400

    Update constants.php

--- workbench/config/constants.php
+++ workbench/config/constants.php
@@ -85,7 +85,7 @@ $GLOBALS["MENUS"] = array(
         'future_get.php'              => new Page('Future Get','Future Get',true,false,false,false,false,'')
      ),
     
-    'Logins' => array(
+    'Logins' => (
         'SFLogins.php'          => new Page('SF Logins','SF Logins',true,true,true,false,true,''),
     )
 );
