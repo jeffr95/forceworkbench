e44f1e9acf6b354f00068cb7fd5b88f8cdc25876
mjbean <32068678+mjbean@users.noreply.github.com>
2017-10-02 08:57:17 -0400

    Update SFLogins.php

--- workbench/SFLogins.php
+++ workbench/SFLogins.php
@@ -4,13 +4,19 @@ require_once 'shared.php';
 require_once 'header.php';
 ?>
 
+<style>
+table, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+</style>
 
 <form>
-  <table style="width:100%; border: 1px solid black;">
-  <h1>RelationEdge Projects</h1>
+  <table style="width:100%">
+  <h4>RelationEdge Projects</h1>
   <tr>
-    <td style="border: 1px solid black;"><input type="button" value="Login" onclick="window.open('https://www.salesforce.com/login.jsp?un=relationedgeatlanta@gmail.com&pw=Atlanta12345')"/>     RelationEdge Prod</td>
-    <td style="border: 1px solid black;"><input type="button" value="Login" onclick="window.open('https://test.salesforce.com/login.jsp?un=jrichards@relationedge.dev1.com&pw=REPassword1')"/>     RelationEdge Sandbox</td>
+    <td><input type="button" value="Login" onclick="window.open('https://www.salesforce.com/login.jsp?un=relationedgeatlanta@gmail.com&pw=Atlanta12345')"/>     RelationEdge Prod</td>
+    <td><input type="button" value="Login" onclick="window.open('https://test.salesforce.com/login.jsp?un=jrichards@relationedge.dev1.com&pw=REPassword1')"/>     RelationEdge Sandbox</td>
   </tr>
   </table>
 </form>
