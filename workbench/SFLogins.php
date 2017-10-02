a57a11cd205a851eb81cfecaf17e48aca2156964
mjbean <32068678+mjbean@users.noreply.github.com>
2017-10-02 08:49:07 -0400

    Update SFLogins.php

--- workbench/SFLogins.php
+++ workbench/SFLogins.php
@@ -6,6 +6,10 @@ require_once 'header.php';
 
 
 <form>
-  <p><input type="button" value="Login" onclick="window.open('https://www.salesforce.com/login.jsp?un=relationedgeatlanta@gmail.com&pw=Atlanta12345')"/>     RelationEdge Prod</p>
-  <p><input type="button" value="Login" onclick="window.open('https://test.salesforce.com/login.jsp?un=jrichards@relationedge.dev1.com&pw=REPassword1')"/>     RelationEdge Sandbox</p>
+  <table style="width:100%">
+  <tr>
+    <td><p><input type="button" value="Login" onclick="window.open('https://www.salesforce.com/login.jsp?un=relationedgeatlanta@gmail.com&pw=Atlanta12345')"/>     RelationEdge Prod</p></td>
+    <td><p><input type="button" value="Login" onclick="window.open('https://test.salesforce.com/login.jsp?un=jrichards@relationedge.dev1.com&pw=REPassword1')"/>     RelationEdge Sandbox</p></td>
+  </tr>
+  </table>
 </form>
