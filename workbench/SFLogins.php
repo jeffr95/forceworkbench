7b0dacb1a7db64c90a2a97cf001a2fc782c7498d
mjbean <32068678+mjbean@users.noreply.github.com>
2017-10-02 09:06:57 -0400

    Update SFLogins.php

--- workbench/SFLogins.php
+++ workbench/SFLogins.php
@@ -15,8 +15,8 @@ table#t01, table#t01 td{
   <table id="t01" style="width:100%">
   <h4>RelationEdge Projects</h1>
   <tr>
-      <th>Production</th>
-      <th>Sandbox</th>
+      <th style="border:none; text-align:center">Production</th>
+      <th style="border:none; text-align:center">Sandbox</th>
   </tr>
   <tr>
     <td><input type="button" value="Login" onclick="window.open('https://www.salesforce.com/login.jsp?un=relationedgeatlanta@gmail.com&pw=Atlanta12345')"/>     RelationEdge Prod</td>
