6a3c139d9fccf3f55290fcfdfccf943126d729ee
mjbean <32068678+mjbean@users.noreply.github.com>
2017-10-02 09:00:28 -0400

    Update SFLogins.php

--- workbench/SFLogins.php
+++ workbench/SFLogins.php
@@ -5,14 +5,14 @@ require_once 'header.php';
 ?>
 
 <style>
-table, td {
+table#t01, table#t01 td{
     border: 1px solid black;
     border-collapse: collapse;
 }
 </style>
 
 <form>
-  <table style="width:100%">
+  <table id="t01" style="width:100%">
   <h4>RelationEdge Projects</h1>
   <tr>
     <td><input type="button" value="Login" onclick="window.open('https://www.salesforce.com/login.jsp?un=relationedgeatlanta@gmail.com&pw=Atlanta12345')"/>     RelationEdge Prod</td>
